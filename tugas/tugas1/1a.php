8605cb3c26b6f2436e0515bfc8e68da0e77bc2b3
maelaniningrum <maelaniningrum@gmail.com>
2024-02-19 14:24:01 +0700

    first commit

--- tugas/tugas1/1a.php
+++ tugas/tugas1/1a.php
@@ -2,5 +2,5 @@
 $topi ="Topi";
 $bundar ="Bundar";
 
-echo "<i><h2>" . '"' . "$topi saya $bundar, $bundar $topi saya" . '"' . "<?h2></i>"
+echo "<i><h2>" . '"' . "$topi saya $bundar, $bundar $topi saya" . '"' . "<?h2></i>";
 ?>
