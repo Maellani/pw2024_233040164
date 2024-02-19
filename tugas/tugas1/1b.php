83e53a20a646728d36a6bf558eaab0db810fdfeb
maelaniningrum <maelaniningrum@gmail.com>
2024-02-19 14:20:03 +0700

    first commit

---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,15 @@
+<?php
+
+$angka = 64;
+
+print "Aku adalah <b>$angka</b><br>";
+
+$angka *= 5;
+print "Jika aku dikali 5, maka aku sekarang menjadi <b> $angka </b> <br>";
+$angka /= 2;
+print "Jika aku dibagi 2, maka aku sekarang menjadi <b> $angka </b> <br>";
+$angka += 75;
+print "Jika aku ditambah 75, maka aku sekarang menjadi <b> $angka </b> <br>";
+$angka -= 20;
+print "Jika aku dikurang 20, maka aku sekarang menjadi <b> $angka </b> <br>";
+?>
