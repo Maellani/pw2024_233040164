e40df8aa51bdea4ad1fdcb170c537ac860d2c326
maelaniningrum <maelaniningrum@gmail.com>
2024-03-07 19:37:27 +0700

    Mengerjakan Tugas

---
+++ tugas/tugas4/4b.php
@@ -0,0 +1,32 @@
+<?php
+
+$hardware = ["Motherboard", "Processor", "Hard Disk", "PC Coller", "VGA Card", "SSD"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 4b</title>
+</head>
+<body>
+    <h3>Macam-macam perangkat keras komputer</h3>
+    <ol>
+        <?php foreach ($hardware as $h) : ?>
+        <li><?= $h ?></li>
+        <?php endforeach; ?>
+    </ol>
+
+    <?php
+    array_push($hardware, "Card Reader", "Modem");
+    sort($hardware) ?>
+    <h3>Macam-macam perangkat keras komputer baru</h3>
+    <ol>
+        <?php for ($i = 0; $i < count($hardware); $i++) { ?>
+            <li><?= $hardware[$i]; ?></li>
+        <?php } ?>
+    </ol>
+
+</body>
+</html>
